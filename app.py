a8f5c0f47b9a5cd35eb34a54f636a79ec98a7c9e
FilipFG7 <80648662+FilipFG7@users.noreply.github.com>
2022-03-27 20:40:36 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,17 @@
+#imports
 import dash
 from dash import Dash, html, dcc
 import pandas as pd
 import plotly.express as px
 
+#reading the excel and the 4 sheets
 githubpath = './data/'
 df_customers = pd.read_excel(githubpath + 'my_shop_data.xlsx', sheet_name="customers")
 df_order = pd.read_excel(githubpath + 'my_shop_data.xlsx', sheet_name="order")
 df_employee = pd.read_excel(githubpath + 'my_shop_data.xlsx', sheet_name="employee")
 df_products = pd.read_excel(githubpath + 'my_shop_data.xlsx', sheet_name="products")
 
+#function for combining tables and calcualting sales
 def get_data():
     df_employee['Employee_Names'] = df_employee['firstname'] + ' ' + df_employee['lastname']
     df_order['Sales'] = df_order['unitprice'] * df_order['quantity']
@@ -19,6 +22,7 @@ def get_data():
 
     return order
 
+#Creating graphs
 order = get_data()
 fig_product=px.bar(order,
     x='productname', y='Sales',
@@ -30,6 +34,7 @@ fig_employee = px.bar(order,
     color='type', title='Sales by Employee',
     labels={'Sales':'Total Sales', 'Employee_Names':'Employee', 'type':'Product Type'})
 
+#staring dash
 app = Dash(__name__)
 
 app.layout = html.Div(children=[
@@ -46,5 +51,6 @@ app.layout = html.Div(children=[
             id='graph2',
             figure=fig_employee),])])
 
+#running dash 
 if __name__ == '__main__':
     app.run_server(debug=True, use_reloader=False)
